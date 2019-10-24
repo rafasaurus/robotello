e6bcae2c612bbdce57256ce66218a6d90c4a5957
rafasaurus <rafa.grigorian@gmail.com>
2019-10-24 20:33:52 +0400

    Add clockwise and anticlockwise motor movements

--- src/__Move__.h
+++ src/__Move__.h
@@ -26,14 +26,14 @@ class __Move__ {
         int motorDelayTime_;
     public:
         __Move__(int step_pin_x_, 
-                    int step_pin_y_, 
-                    int step_pin_z_, 
-                    int step_pin_a_,
-                    int dir_pin_x_,
-                    int dir_pin_y_,
-                    int dir_pin_z_,
-                    int dir_pin_a_,
-                    int motorDelayTime_) {
+                int step_pin_y_, 
+                int step_pin_z_, 
+                int step_pin_a_,
+                int dir_pin_x_,
+                int dir_pin_y_,
+                int dir_pin_z_,
+                int dir_pin_a_,
+                int motorDelayTime_) {
             this->step_pin_x_ = step_pin_x_;
             this->step_pin_y_ = step_pin_y_;
             this->step_pin_z_ = step_pin_z_;
@@ -55,38 +55,38 @@ class __Move__ {
 
         }
         void changeDirForward() {
-          digitalWrite(this->dir_pin_x_, 1);
-          digitalWrite(this->dir_pin_y_, 1);
-          digitalWrite(this->dir_pin_z_, 1);
-          digitalWrite(this->dir_pin_a_, 1);
+            digitalWrite(this->dir_pin_x_, 1);
+            digitalWrite(this->dir_pin_y_, 1);
+            digitalWrite(this->dir_pin_z_, 1);
+            digitalWrite(this->dir_pin_a_, 1);
         }
 
         void changeDirBackward() {
-          digitalWrite(this->dir_pin_x_, 0);
-          digitalWrite(this->dir_pin_y_, 0);
-          digitalWrite(this->dir_pin_z_, 0);
-          digitalWrite(this->dir_pin_a_, 0);
+            digitalWrite(this->dir_pin_x_, 0);
+            digitalWrite(this->dir_pin_y_, 0);
+            digitalWrite(this->dir_pin_z_, 0);
+            digitalWrite(this->dir_pin_a_, 0);
         }
 
         void changeDirRight() {
-          digitalWrite(this->dir_pin_x_, 0);
-          digitalWrite(this->dir_pin_y_, 0);
-          digitalWrite(this->dir_pin_z_, 1);
-          digitalWrite(this->dir_pin_a_, 1);
+            digitalWrite(this->dir_pin_x_, 0);
+            digitalWrite(this->dir_pin_y_, 0);
+            digitalWrite(this->dir_pin_z_, 1);
+            digitalWrite(this->dir_pin_a_, 1);
         }
 
         void changeDirLeft() {
-          digitalWrite(this->dir_pin_x_, 1);
-          digitalWrite(this->dir_pin_y_, 1);
-          digitalWrite(this->dir_pin_z_, 0);
-          digitalWrite(this->dir_pin_a_, 0);
+            digitalWrite(this->dir_pin_x_, 1);
+            digitalWrite(this->dir_pin_y_, 1);
+            digitalWrite(this->dir_pin_z_, 0);
+            digitalWrite(this->dir_pin_a_, 0);
         }
 
         void goLeft() {
-          digitalWrite(this->dir_pin_x_, 0);
-          digitalWrite(this->dir_pin_y_, 1);
-          digitalWrite(this->dir_pin_z_, 1);
-          digitalWrite(this->dir_pin_a_, 0);
+            digitalWrite(this->dir_pin_x_, 0);
+            digitalWrite(this->dir_pin_y_, 1);
+            digitalWrite(this->dir_pin_z_, 1);
+            digitalWrite(this->dir_pin_a_, 0);
         }
         void one_step() {
             digitalWrite(this->step_pin_x_, HIGH);
@@ -100,23 +100,32 @@ class __Move__ {
             digitalWrite(this->step_pin_a_, LOW);
             delayMicroseconds(this->motorDelayTime_); 
         }
-        
-        void n_step(int steps) {
-           for (int step = 0; step <= steps; ++step) {
-             one_step();
-           }
+        // TODO 
+        // with parameter delayDifference 
+        // smooth start, smooth stop
+        void one_step_clockwise() {
+            digitalWrite(this->step_pin_x_, HIGH);
+            digitalWrite(this->step_pin_y_, HIGH);
+            delayMicroseconds(this->motorDelayTime_); 
+            digitalWrite(this->step_pin_x_, LOW);
+            digitalWrite(this->step_pin_y_, LOW);
+            delayMicroseconds(this->motorDelayTime_); 
         }
-        void n_step_smooth(int steps) {
-           // TODO map(steps)
-           for (int step = 0; step <= steps; ++step) {
-             if (step < 3000) {
-                this->motorDelayTime_ += int(step*0.00007);
+        void one_step_anticlockwise() {
+            digitalWrite(this->step_pin_z_, HIGH);
+            digitalWrite(this->step_pin_a_, HIGH);
+            delayMicroseconds(this->motorDelayTime_); 
+            digitalWrite(this->step_pin_z_, LOW);
+            digitalWrite(this->step_pin_a_, LOW);
+            delayMicroseconds(this->motorDelayTime_); 
+        }
+        void n_step(int steps) {
+            for (int step = 0; step <= steps; ++step) {
                 one_step();
-             } else {
-                this->motorDelayTime_ -= int(step*0.00007);
-                one_step();
-             }
-           }
+                one_step_clockwise();
+                one_step_clockwise();
+                one_step_clockwise();
+            }
         }
         void changeMotorDelay(int del) {
             this->motorDelayTime_ = del;
--- src/src.ino
+++ src/src.ino
@@ -3,6 +3,9 @@
 #include "__Move__.h"
 #include "__Ultrasonic__.h"
 
+#define rightSensorPin A0 // RightSesnor
+#define leftSensorPin A1 // LeftSesnor
+
 __Servo__ *arm;
 __Servo__ *armOne;
 __Servo__ *armTwo;
@@ -14,26 +17,22 @@ void setup() {
     armTwo = new __Servo__(0, 168, 30, 3);
     armOne = new __Servo__(0, 180, 30,4);
     armRotate = new __Servo__(2);
+
     motor = new __Move__(X_STP,
-                        Y_STP,
-                        Z_STP,
-                        A_STP,
-                        X_DIR,
-                        Y_DIR, 
-                        Z_DIR,
-                        A_DIR,
-                        100);
+            Y_STP,
+            Z_STP,
+            A_STP,
+            X_DIR,
+            Y_DIR, Z_DIR,
+            A_DIR,
+            100);
     ultrasonic = new __Ultrasonic__();
     Serial.begin(115200);
     Serial.print("setup complete");
+    motor->changeDirForward();
 }
 
 void loop() {
-    armRotate->setAngle(130);
-    motor->changeDirForward();
-    motor->n_step_smooth(6000);
-    motor->changeDirRight();
-    motor->n_step_smooth(6000);
-    int debug = ultrasonic->getDistance();
-    Serial.print(debug);
+    motor->one_step();
+    motor->one_step_clockwise();
 }
