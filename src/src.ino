155b6eaa5e235f6ccd74b6e46f8a108b1bfe6c69
rafasaurus <rafa.grigorian@gmail.com>
2019-10-28 13:04:59 +0400

    Refactor

--- src/src.ino
+++ src/src.ino
@@ -6,33 +6,27 @@
 #define rightSensorPin A0 // RightSesnor
 #define leftSensorPin A1 // LeftSesnor
 
-__Servo__ *arm;
-__Servo__ *armOne;
-__Servo__ *armTwo;
-__Servo__ *armRotate;
-__Move__ *motor;
-__Ultrasonic__ *ultrasonic;
+__Servo__ arm(0, 60, 15, 5);
+__Servo__ armTwo(0, 168, 30, 3);
+__Servo__ armOne(0, 180, 30,4);
+__Servo__ armRotate(2);
+__Move__ motor(X_STP,
+        Y_STP,
+        Z_STP,
+        A_STP,
+        X_DIR,
+        Y_DIR, Z_DIR,
+        A_DIR,
+        100);
+__Ultrasonic__ ultrasonic;
 void setup() {
-    arm = new __Servo__(0, 60, 15, 5);
-    armTwo = new __Servo__(0, 168, 30, 3);
-    armOne = new __Servo__(0, 180, 30,4);
-    armRotate = new __Servo__(2);
-
-    motor = new __Move__(X_STP,
-            Y_STP,
-            Z_STP,
-            A_STP,
-            X_DIR,
-            Y_DIR, Z_DIR,
-            A_DIR,
-            100);
-    ultrasonic = new __Ultrasonic__();
     Serial.begin(115200);
     Serial.print("setup complete");
-    motor->changeDirForward();
+    motor.changeDirForward();
 }
 
 void loop() {
-    motor->one_step();
-    motor->one_step_clockwise();
+    if (ultrasonic.getDistance() > 250) {
+        motor.one_step();
+    }
 }
