f9f7b8bbdd6f97a498e8411ac73be5fa1eaefacf
rafasaurus <rafa.grigorian@gmail.com>
2019-10-29 09:26:08 +0400

    Add colorSense lib

---
+++ src/__ColorSense__.h
@@ -0,0 +1,56 @@
+#define S0 45
+#define S1 44
+#define S2 42
+#define S3 43
+#define sensorOut 9
+
+class __ColorSense__ {
+    private:
+        int frequency = 0;
+    public:
+        // constructor
+        __ColorSense__() {
+            pinMode(S0, OUTPUT);
+            pinMode(S1, OUTPUT);
+            pinMode(S2, OUTPUT);
+            pinMode(S3, OUTPUT);
+            pinMode(sensorOut, INPUT);
+            // Setting frequency-scaling to 20%
+            digitalWrite(S0,HIGH);
+            digitalWrite(S1,LOW);
+        }
+        void getColor() {
+            // Setting red filtered photodiodes to be read
+            digitalWrite(S2,LOW);
+            digitalWrite(S3,LOW);
+            // Reading the output frequency
+            this->frequency = pulseIn(sensorOut, LOW);
+            // Printing the value on the serial monitor
+            // Serial.print("R= ");//printing name
+            // Serial.print(this->frequency);//printing RED color frequency
+            // Serial.print("  ");
+            delay(10);
+
+            // Setting Green filtered photodiodes to be read
+            digitalWrite(S2,HIGH);
+            digitalWrite(S3,HIGH);
+            // Reading the output this->frequency
+            this->frequency = pulseIn(sensorOut, LOW);
+            // Printing the value on the serial monitor
+            // Serial.print("G= ");//printing name
+            // Serial.print(this->frequency);//printing RED color this->frequency
+            // Serial.print("  ");
+            delay(10);
+
+            // Setting Blue filtered photodiodes to be read
+            digitalWrite(S2,LOW);
+            digitalWrite(S3,HIGH);
+            // Reading the output frequency
+            this->frequency = pulseIn(sensorOut, LOW);
+            // Printing the value on the serial monitor
+            // Serial.print("B= ");//printing name
+            // Serial.print(this->frequency);//printing RED color frequency
+            // Serial.println("  ");
+            delay(10);
+        }
+};
